5c19f30c35ad9963a573997d4a5efe02c5e1fe83
João Henrique <joaohscorrea@outlook.com>
2019-12-19 00:38:27 -0300

    #03 - Ajustes na responsividade de contato e curso

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,13 @@
 $('.responsive').slick({
     dots: true,
-    infinite: true,
+    infinite: true  ,
     arrows:false,
+    autoplay:true,
+    autoplaySpeed: 2000,
     speed: 300,
     slidesToShow: 3,
     slidesToScroll: 3,
     responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true
-        }
-      },
       {
         breakpoint: 600,
         settings: {
@@ -23,7 +16,7 @@ $('.responsive').slick({
         }
       },
       {
-        breakpoint: 480,
+        breakpoint: 368,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
